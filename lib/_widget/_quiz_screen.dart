33b8f439b06b61999ed71d014af90eecd31fe2a8
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-06-22 02:37:23 -0400

    add main

--- lib/_widget/_quiz_screen.dart
+++ lib/_widget/_quiz_screen.dart
@@ -20,7 +20,203 @@ class QuestionListQuestionQuiz extends StatefulWidget {
       _QuestionListQuestionQuizState();
 }
 
+
+
 class _QuestionListQuestionQuizState extends State<QuestionListQuestionQuiz> {
+  int currentQuestionIndex = 0;
+  int? selectedAnswerIndex;
+  int score = 0;
+  bool showCorrectAnswer = false;
+  bool showNextButton = false;
+  double progress = 0.0;
+
+  void checkAnswer() {
+    int correctAnswerIndex =
+        widget.questions[currentQuestionIndex].correctAnswerIndex;
+    if (selectedAnswerIndex == correctAnswerIndex) {
+      score++;
+    }
+    showCorrectAnswer = true;
+    showNextButton = true;
+    setState(() {});
+  }
+
+  void showNextQuestion() {
+    selectedAnswerIndex = null;
+    showCorrectAnswer = false;
+    showNextButton = false;
+    progress = (currentQuestionIndex + 1) / widget.questions.length;
+
+    if (currentQuestionIndex < widget.questions.length - 1) {
+      currentQuestionIndex++;
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ResultScreen(
+            score: score,
+            totalQuestions: widget.questions.length,
+            titleQuiz: widget.questionModel.quizTitle,
+          ),
+        ),
+      );
+    }
+    setState(() {});
+  }
+
+  Future<bool?> _confirmExitQuiz() async {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Quit Quiz'),
+        content: Text('Are you sure you want to quit the quiz? Your progress will be lost.'),
+        actions: [
+          TextButton(
+            onPressed: () => {},
+            child: Text('No'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<bool> _onWillPop() async {
+    bool? shouldPop = await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmation'),
+          content: const Text('Voulez-vous vraiment quitter le quiz ? Vos modifications seront perdues.'),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(false); // L'utilisateur choisit de ne pas quitter
+              },
+              child: const Text('Non'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(true); // L'utilisateur choisit de quitter
+              },
+              child: const Text('Oui'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (shouldPop == true) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()),
+      );
+    }
+
+    return Future.value(shouldPop ?? false); // Retourne false par défaut si la boîte de dialogue est fermée sans sélection
+  }
+
+ /*   Future<bool> _onWillPop() async {
+    if (currentQuestionIndex == 0) {
+      final confirmExit = await _confirmExitQuiz();
+      if (confirmExit!) {
+        Navigator.push(
+          context,
+           MaterialPageRoute(
+          builder: (context) => MainScreen(),
+        ),
+      );
+    }
+    return !confirmExit;
+  } else {
+    // showPreviousQuestion();
+    return false;
+  }
+}
+ */
+  double getScorePercentage() {
+    double percentage = (score / widget.questions.length) * 100;
+    return percentage;
+  }
+
+  Future<void> pauseAndContinue() async {
+    await Future.delayed(const Duration(seconds: 2));
+    showNextQuestion();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Quiz App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: widget.questions.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    LinearProgressIndicator(
+                      value: progress,
+                      color: Colors.blue,
+                      backgroundColor: Colors.pink,
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      'Question ${currentQuestionIndex + 1}/${widget.questions.length}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 16),
+                    QuestionSingle(
+                      question: widget.questions[currentQuestionIndex],
+                      selectedAnswerIndex: selectedAnswerIndex,
+                      showCorrectAnswer: showCorrectAnswer,
+                      onAnswerSelected: (index) {
+                        setState(() {
+                          selectedAnswerIndex = index;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    if (!showNextButton)
+                      ElevatedButton(
+                        onPressed: selectedAnswerIndex == null || showCorrectAnswer
+                            ? null
+                            : () {
+                                checkAnswer();
+                              },
+                        child: const Text('Check Answer'),
+                      ),
+                    const SizedBox(height: 16),
+                    if (showNextButton)
+                      ElevatedButton(
+                        onPressed: () {
+                          showNextQuestion();
+                        },
+                        child: const Text('Next Question'),
+                      ),
+                    const SizedBox(height: 16),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/* class _QuestionListQuestionQuizState extends State<QuestionListQuestionQuiz> {
   int currentQuestionIndex = 0;
   int? selectedAnswerIndex;
   int score = 0;
@@ -176,3 +372,4 @@ class _QuestionListQuestionQuizState extends State<QuestionListQuestionQuiz> {
     super.dispose();
   }
 }
+ */
--- lib/_widget/_widget_question.dart
+++ lib/_widget/_widget_question.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_quiz/model/question.dart';
 
 
-class QuestionSingle extends StatelessWidget {
+/* class QuestionSingle extends StatelessWidget {
   final Question question;
   final int? selectedAnswerIndex;
   final bool showCorrectAnswer;
@@ -59,4 +59,135 @@ class QuestionSingle extends StatelessWidget {
     ),
     );
   }
+} */
+
+class QuestionSingle extends StatelessWidget {
+  final Question question;
+  final int? selectedAnswerIndex;
+  final bool showCorrectAnswer;
+  final Function(int) onAnswerSelected;
+
+  const QuestionSingle({
+    Key? key,
+    required this.question,
+    required this.selectedAnswerIndex,
+    required this.showCorrectAnswer,
+    required this.onAnswerSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 16),
+          Text(
+            question.question,
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: question.options.length,
+            itemBuilder: (context, index) {
+              final optionText = question.options[index];
+              bool isCorrectAnswer = index == question.correctAnswerIndex;
+              bool isSelectedAnswer = index == selectedAnswerIndex;
+
+              return GestureDetector(
+                onTap: () {
+                  if (!showCorrectAnswer) {
+                    onAnswerSelected(index);
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: showCorrectAnswer
+                        ? isCorrectAnswer
+                            ? Colors.green.withOpacity(0.5)
+                            : isSelectedAnswer
+                                ? Colors.red.withOpacity(0.5)
+                                : null
+                        : null,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: RadioListTile<int>(
+                    title: Text(
+                      optionText,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: isSelectedAnswer
+                            ? Colors.blue
+                            : showCorrectAnswer && isCorrectAnswer
+                                ? Colors.green
+                                : null,
+                      ),
+                    ),
+                    value: index,
+                    groupValue: selectedAnswerIndex,
+                    onChanged: (value) {
+                      if (!showCorrectAnswer) {
+                        onAnswerSelected(value ?? 0);
+                      }
+                    },
+                    activeColor: Colors.blue, // Couleur du bouton radio sélectionné
+                  ),
+                ),
+              );
+            },
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
 }
+
+
+/* class QuestionSingle extends StatelessWidget {
+  final Question question;
+  final int? selectedAnswerIndex;
+  final bool showCorrectAnswer;
+  final Function(int) onAnswerSelected;
+
+  const QuestionSingle({
+    Key? key,
+    required this.question,
+    required this.selectedAnswerIndex,
+    required this.showCorrectAnswer,
+    required this.onAnswerSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 16),
+          Text(
+            question.question,
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: question.options.length,
+            itemBuilder: (context, index) {
+              return RadioListTile<int>(
+                title: Text(
+                  question.options[index],
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: selectedAnswerIndex == index ? Colors.blue : null,
+                  ),
+                ),
+                value: index,
+                groupValue: selectedAnswerIndex,
+                onChanged:
+
+ */
