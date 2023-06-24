718f5ea2e0f1b2ce3ee578336befb398537ea6bf
Djeevs-Jean <jeanyvesbadette@gmail.com>
2023-06-23 20:43:57 -0400

    version radioList languages

--- lib/main.dart
+++ lib/main.dart
@@ -51,13 +51,14 @@ class _MainScreenState extends State<MainScreen> {
   ];
    @override
   Widget build(BuildContext context) {
+    String titleAppbar = 'apptitle'.tr();
     // context.locale = Locale('en', 'HT');
 
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('apptitle'.tr().toString()),
+          title: Text(titleAppbar),
           
           // title: const Text('Quiz App'),
           bottom: TabBar(tabs: tabs),
@@ -66,8 +67,8 @@ class _MainScreenState extends State<MainScreen> {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            const UserAccountsDrawerHeader(
-              accountName: Text('Quiz App'),
+            UserAccountsDrawerHeader(
+              accountName: Text(titleAppbar),
               accountEmail: Text('quizapp@dev.com'),
                 currentAccountPicture: CircleAvatar(
                   backgroundImage: AssetImage('assets/images/quiz1.jpg'),
@@ -76,35 +77,7 @@ class _MainScreenState extends State<MainScreen> {
                   color: Colors.blue,
                 ),
               ),
-              /* ListTile(
-                leading: const Icon(Icons.login),
-                title: const Text('Se connecter'),
-                onTap: () {
-                  // Code pour naviguer vers la page de connexion
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Accueil'),
-                onTap: () {
-                  // Code pour naviguer vers la page d'accueil
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.logout),
-                title: const Text('Se déconnecter'),
-                onTap: () {
-                  // Code pour déconnecter l'utilisateur
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.person_add),
-                title: const Text('Créer un compte'),
-                onTap: () {
-                  // _onItemTapped(1);
-                  Navigator.pop(context);
-                },
-              ), */
+              
               ListTile(
                 leading: const Icon(Icons.language),
                 title: Text('Choisir la langue'),
@@ -118,39 +91,6 @@ class _MainScreenState extends State<MainScreen> {
                 },
               ),
 
-              /*ListTile(
-                 leading: Icon(Icons.language),
-                title: Text('Change Language'),
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('Select Language'),
-                        content: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            ListTile(
-                              title: Text('English'),
-                              onTap: () {
-                                EasyLocalization.of(context).locale = Locale('en', 'US');
-                                Navigator.pop(context);
-                              },
-                            ),
-                            ListTile(
-                              title: Text('French'),
-                              onTap: () {
-                                EasyLocalization.of(context).locale = Locale('fr', 'FR');
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                },
-              ), */
             ],
           ),
         ),
@@ -161,7 +101,61 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 }
+class LanguageSelectionDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return SimpleDialog(
+                  title: const Text('Choisir la langue'),
+                  children: [
+                    RadioListTile(
+                      title: const Text('Kreyol'),
+                      value: 'HT',
+                      groupValue: context.locale.countryCode, // Utilise le code de pays de la langue actuelle comme valeur de groupe
+                      onChanged: (value) {
+                        // Met à jour la langue vers le créole haïtien
+                        context.setLocale(Locale('en', 'HT'));
+                        Navigator.of(context).pop(); // Ferme le dialogue
+                        Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(builder: (context) => MainScreen()),
+                          (route) => false,
+                        ); // Navigue vers MainScreen et ferme les autres routes
+                      },
+                    ),
+                    RadioListTile(
+                      title: const Text('English'),
+                      value: 'US',
+                      groupValue: context.locale.countryCode, // Utilise le code de pays de la langue actuelle comme valeur de groupe
+                      onChanged: (value) {
+                        // Met à jour la langue vers l'anglais
+                        context.setLocale(Locale('en', 'US'));
+                        Navigator.of(context).pop(); // Ferme le dialogue
+                        Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(builder: (context) => MainScreen()),
+                          (route) => false,
+                        ); // Navigue vers MainScreen et ferme les autres routes
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: Text('Langue actuelle: ${context.locale.languageCode}-${context.locale.countryCode}'), // Affiche la langue actuelle
+        ),
+      ],
+    );
+  }
+}
 
+
+/* 
 class LanguageSelectionDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -169,24 +163,25 @@ class LanguageSelectionDialog extends StatelessWidget {
       title: const Text('Choisir la langue'),
       children: [
         RadioListTile(
-          title: const Text('Français'),
-          value: 'fr',
-          groupValue: 'currentLanguage', // Replace 'currentLanguage' with your current language variable
+          title: const Text('Kreyol'),
+          value: 'HT',
+          groupValue: context.locale.languageCode, // Utilise la langue actuelle comme valeur de groupe
           onChanged: (value) {
-            // Update the language to French
-            // Example code: languageController.setLanguage('fr');
+            // Met à jour la langue vers le français
+            context.setLocale(Locale('en', value));
           },
         ),
         RadioListTile(
           title: const Text('English'),
-          value: 'en',
-          groupValue: 'currentLanguage', // Replace 'currentLanguage' with your current language variable
+          value: 'US',
+          groupValue: context.locale.languageCode, // Utilise la langue actuelle comme valeur de groupe
           onChanged: (value) {
-            // Update the language to English
-            // Example code: languageController.setLanguage('en');
+            // Met à jour la langue vers l'anglais
+            context.setLocale(Locale('en', value));
           },
         ),
       ],
     );
   }
 }
+ */
